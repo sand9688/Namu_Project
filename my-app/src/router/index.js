99921f7bcc30852cfd77517a22426afef7123690
sand9688 <sand9688@naver.com>
2022-12-28 21:14:38 +0900

    test

--- my-app/src/router/index.js
+++ my-app/src/router/index.js
@@ -14,7 +14,18 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import(/* webpackChunkName: "about" */ '../views/LoginView.vue')
+  },
+  {
+    path: '/create',
+    name: 'create',
+    component: () => import(/* webpackChunkName: "about" */ '../views/CreateView.vue')
   }
+
 ]
 
 const router = createRouter({
